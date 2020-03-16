9d7bfe0fdf5bbf60f2a1b74bec8f82f02ec71737
paisen4312 <61623989+paisen4312@users.noreply.github.com>
2020-03-16 23:37:11 +0900

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var header = $('<div>').addClass("contents").append('<a href="index.html">TOP PAGE</a>')
+.append('<a href="comingsoon.html">EVENTS</a>')
+.append('<a href="member.html">MEMBERS</a>')
+.append('<a href="comingsoon.html">NEWS</a>')
+.append('<a href="comingsoon.html">RESULT</a>')
+.append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a>')
+.append('<a href="https://www.picuki.com/search/@ls_waseda">INSTAGRAM</a>')
+.append('<a href="history.html">HISTORY</a>')
+
+var footer = $('<div>').addClass("contents").append('<a href="index.html">TOP PAGE</a>')
+.append('<a href="comingsoon.html">EVENTS</a>')
+.append('<a href="member.html">MEMBERS</a>')
+.append('<a href="comingsoon.html">NEWS</a>')
+.append('<a href="comingsoon.html">RESULT</a>')
+.append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a>')
+.append('<a href="https://www.picuki.com/search/@ls_waseda">INSTAGRAM</a>')
+.append('<a href="history.html">HISTORY</a>')
+
+$('#header').append(header);
+$('#footer').append(footer);
