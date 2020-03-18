8649a64a0065153cdb69ea9057461c0a5cc8e20f
paisen4312 <61623989+paisen4312@users.noreply.github.com>
2020-03-19 02:06:19 +0900

    Update member.js

--- js/member.js
+++ js/member.js
@@ -8,23 +8,22 @@ $('.grade').click(function(){
     $('#members').fadeOut(1500)
     $('.selected-header').fadeOut(1500)
     var grade = $(this).attr("id");
-    setTimeout( "changeMember(grade)" ,2000 )
+    setTimeout( "changeMember(grade)" ,1000 )
 
+    function changeMember(grade){
+
+        var leftPicRow = $('<img>').addClass("selected-header first-pic").attr("src" , "img/" + grade + "/1.jfif");
+        $('.first-pic').replaceWith(leftPicRow);
+
+        var row = $('<p>').addClass("selected-header selected-main").text(grade);
+        $('.selected-main').replaceWith(row);
+
+        var rightPicRow = $('<img>').addClass("selected-header second-pic").attr("src" , "img/" + grade + "/2.jfif");
+        $('.second-pic').replaceWith(rightPicRow);
+
+        $('#members').load("member/" + grade + ".html");
+        $('#regends').load("member/" + grade + ".html");
+        $('#members').fadeIn(1500)
+        $('.selected-header').fadeIn(1500)
+    };
 });
-
-function changeMember(grade){
-
-    var leftPicRow = $('<img>').addClass("selected-header first-pic").attr("src" , "img/" + grade + "/1.jfif");
-    $('.first-pic').replaceWith(leftPicRow);
-
-    var row = $('<p>').addClass("selected-header selected-main").text(grade);
-    $('.selected-main').replaceWith(row);
-    
-    var rightPicRow = $('<img>').addClass("selected-header second-pic").attr("src" , "img/" + grade + "/2.jfif");
-    $('.second-pic').replaceWith(rightPicRow);
-
-    $('#members').load("member/" + grade + ".html");
-    $('#regends').load("member/" + grade + ".html");
-    $('#members').fadeIn(1500)
-    $('.selected-header').fadeIn(1500)
-};
