2f652404e56c996a79eb83298344337fc9b5f376
paisen4312 <61623989+paisen4312@users.noreply.github.com>
2020-03-18 09:13:02 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ var header = $('<div>').addClass("contents").append('<a href="index.html">TOP PA
 .append('<a href="comingsoon.html">NEWS</a>')
 .append('<a href="comingsoon.html">RESULT</a>')
 .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a>')
-.append('<a href="https://www.picuki.com/search/@ls_waseda">INSTAGRAM</a>')
+.append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a>')
 .append('<a href="history.html">HISTORY</a>')
 
 var footer = $('<div>').addClass("contents").append('<a href="index.html">TOP PAGE</a>')
@@ -15,7 +15,7 @@ var footer = $('<div>').addClass("contents").append('<a href="index.html">TOP PA
 .append('<a href="comingsoon.html">NEWS</a>')
 .append('<a href="comingsoon.html">RESULT</a>')
 .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a>')
-.append('<a href="https://www.picuki.com/search/@ls_waseda">INSTAGRAM</a>')
+.append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a>')
 .append('<a href="history.html">HISTORY</a>')
 
 $('#header').append(header);
