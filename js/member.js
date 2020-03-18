4abe85c5989f99324a771033b45a1d2a0a5272eb
paisen4312 <61623989+paisen4312@users.noreply.github.com>
2020-03-19 02:09:54 +0900

    Update member.js

--- js/member.js
+++ js/member.js
@@ -5,25 +5,26 @@ $('#regends').load("member/16th.html");
 
 $('.grade').click(function(){
     
-    $('#members').fadeOut(1500)
-    $('.selected-header').fadeOut(1500)
+    $('#members').fadeOut(1000)
+    $('.selected-header').fadeOut(1000)
     var grade = $(this).attr("id");
-    setTimeout( "changeMember(grade)" ,1000 )
-
-    function changeMember(grade){
-
-        var leftPicRow = $('<img>').addClass("selected-header first-pic").attr("src" , "img/" + grade + "/1.jfif");
-        $('.first-pic').replaceWith(leftPicRow);
-
-        var row = $('<p>').addClass("selected-header selected-main").text(grade);
-        $('.selected-main').replaceWith(row);
-
-        var rightPicRow = $('<img>').addClass("selected-header second-pic").attr("src" , "img/" + grade + "/2.jfif");
-        $('.second-pic').replaceWith(rightPicRow);
-
-        $('#members').load("member/" + grade + ".html");
-        $('#regends').load("member/" + grade + ".html");
-        $('#members').fadeIn(1500)
-        $('.selected-header').fadeIn(1500)
-    };
+    setTimeout('changeMember(grade)' , 2000)
 });
+    
+function changeMember(grade){
+
+    var leftPicRow = $('<img>').addClass("selected-header first-pic").attr("src" , "img/" + grade + "/1.jfif");
+    $('.first-pic').replaceWith(leftPicRow);
+
+    var row = $('<p>').addClass("selected-header selected-main").text(grade);
+    $('.selected-main').replaceWith(row);
+
+    var rightPicRow = $('<img>').addClass("selected-header second-pic").attr("src" , "img/" + grade + "/2.jfif");
+    $('.second-pic').replaceWith(rightPicRow);
+
+    $('#members').load("member/" + grade + ".html");
+    $('#regends').load("member/" + grade + ".html");
+    $('#members').fadeIn(1000)
+    $('.selected-header').fadeIn(1000)
+};
+
