668fd989e3ec203e3a0bd066366cf58bbc5dc9db
paisen4312 <61623989+paisen4312@users.noreply.github.com>
2020-03-18 23:02:45 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -8,6 +8,7 @@ var header = $('<div>').addClass("contents").append('<a href="index.html">TOP PA
 .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a>')
 .append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a>')
 .append('<a href="history.html">HISTORY</a>')
+.append('<div class = "menu" ><p>MENU</p></div>')
 
 var footer = $('<div>').addClass("contents").append('<a href="index.html">TOP PAGE</a>')
 .append('<a href="comingsoon.html">EVENTS</a>')
@@ -18,5 +19,33 @@ var footer = $('<div>').addClass("contents").append('<a href="index.html">TOP PA
 .append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a>')
 .append('<a href="history.html">HISTORY</a>')
 
+  var menuContents = $('<div>').addClass("menu-contents").addClass("hidden-menu").append('<a href="index.html">TOP PAGE</a><br>')
+  .append('<a href="comingsoon.html">EVENTS</a><br>')
+  .append('<a href="member.html">MEMBERS</a><br>')
+  .append('<a href="comingsoon.html">NEWS</a><br>')
+  .append('<a href="comingsoon.html">RESULT</a><br>')
+  .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a><br>')
+  .append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a><br>')
+  .append('<a href="history.html">HISTORY</a>')
+  
+
+
 $('#header').append(header);
 $('#footer').append(footer);
+$('.menu').append(menuContents);
+
+$('.menu').click(function(){
+  
+  if($('.menu-contents').hasClass("hidden-menu")){
+    
+      $('.menu-contents').fadeIn(1000)
+      $('.menu-contents').removeClass("hidden-menu")
+      
+
+  }else{
+  
+    $('.menu-contents').fadeOut(1000)
+    $('.menu-contents').addClass("hidden-menu")
+
+  }
+});
