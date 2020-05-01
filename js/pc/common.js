107d1aabd29b2cdc29e44601da24dcf58eb4be78
paisen4312 <syvlebgth@gmail.com>
2020-05-01 14:17:10 +0900

    コード整理

--- js/pc/common.js
+++ js/pc/common.js
@@ -4,6 +4,45 @@ $('.view-change').load("../common/viewChange.html");
 $('.index-view-change').load("common/indexViewChange.html");
 $('.index-sp-view-change').load("common/indexSpViewChange.html");
 
+
+  var menuContents = $('<div>').addClass("menu-contents").addClass("hidden-menu").append('<a href="index.html">TOPPAGE</a><br>')
+  .append('<a href="events.html">EVENTS</a><br>')
+  .append('<a href="member.html">MEMBERS</a><br>')
+  .append('<a href="comingsoon.html">NEWS</a><br>')
+  .append('<a href="comingsoon.html">RESULTS</a><br>')
+  .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a><br>')
+  .append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a><br>')
+  .append('<a href="history.html">HISTORY</a>')
+
+$('.menu').append(menuContents);
+
+$('.menu').click(function(){
+    if($('.menu-contents').hasClass("hidden-menu")){
+        $('.menu-contents').fadeIn(1000);
+        $('.menu-contents').removeClass("hidden-menu");
+    }else{
+        $('.menu-contents').fadeOut(1000);
+        $('.menu-contents').addClass("hidden-menu");
+    }
+    if($(this).hasClass("back")){
+        $(this).removeClass("back").children('.hub').text('MENU(開く)');
+    }else{
+        $(this).addClass("back").children('.hub').text('MENU(閉じる)') 
+    }
+});
+
+
+
+$('.menu-contents').children('a').hover(
+    function(){
+        $(this).stop(true).animate({color: "red", "font-size": "60px"}, 500);
+    },
+    function(){
+        $(this).stop(true).animate({color: "white", "font-size": "40px"}, 500);
+    }
+);
+
+
 window.onload = function() {
     window.scroll(0,0);
 };
--- js/pc/index.js
+++ js/pc/index.js
@@ -2,42 +2,42 @@
 
 $(function(){
 
-  var menuContents = $('<div>').addClass("menu-contents").addClass("hidden-menu").append('<a href="index.html">TOPPAGE</a><br>')
-  .append('<a href="events.html">EVENTS</a><br>')
-  .append('<a href="member.html">MEMBERS</a><br>')
-  .append('<a href="comingsoon.html">NEWS</a><br>')
-  .append('<a href="comingsoon.html">RESULTS</a><br>')
-  .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a><br>')
-  .append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a><br>')
-  .append('<a href="history.html">HISTORY</a>')
+//   var menuContents = $('<div>').addClass("menu-contents").addClass("hidden-menu").append('<a href="index.html">TOPPAGE</a><br>')
+//   .append('<a href="events.html">EVENTS</a><br>')
+//   .append('<a href="member.html">MEMBERS</a><br>')
+//   .append('<a href="comingsoon.html">NEWS</a><br>')
+//   .append('<a href="comingsoon.html">RESULTS</a><br>')
+//   .append('<a href="https://twitter.com/waseda_ls2013">TWITTER</a><br>')
+//   .append('<a href="https://www.instagram.com/ls_waseda/?hl=ja">INSTAGRAM</a><br>')
+//   .append('<a href="history.html">HISTORY</a>')
 
-$('.menu').append(menuContents);
+// $('.menu').append(menuContents);
 
-$('.menu').click(function(){
-    if($('.menu-contents').hasClass("hidden-menu")){
-        $('.menu-contents').fadeIn(1000);
-        $('.menu-contents').removeClass("hidden-menu");
-    }else{
-        $('.menu-contents').fadeOut(1000);
-        $('.menu-contents').addClass("hidden-menu");
-    }
-    if($(this).hasClass("back")){
-        $(this).removeClass("back").children('.hub').text('MENU(開く)');
-    }else{
-        $(this).addClass("back").children('.hub').text('MENU(閉じる)') 
-    }
-});
+// $('.menu').click(function(){
+//     if($('.menu-contents').hasClass("hidden-menu")){
+//         $('.menu-contents').fadeIn(1000);
+//         $('.menu-contents').removeClass("hidden-menu");
+//     }else{
+//         $('.menu-contents').fadeOut(1000);
+//         $('.menu-contents').addClass("hidden-menu");
+//     }
+//     if($(this).hasClass("back")){
+//         $(this).removeClass("back").children('.hub').text('MENU(開く)');
+//     }else{
+//         $(this).addClass("back").children('.hub').text('MENU(閉じる)') 
+//     }
+// });
 
 
 
-$('.menu-contents').children('a').hover(
-    function(){
-        $(this).stop(true).animate({color: "red", "font-size": "60px"}, 500);
-    },
-    function(){
-        $(this).stop(true).animate({color: "white", "font-size": "40px"}, 500);
-    }
-);
+// $('.menu-contents').children('a').hover(
+//     function(){
+//         $(this).stop(true).animate({color: "red", "font-size": "60px"}, 500);
+//     },
+//     function(){
+//         $(this).stop(true).animate({color: "white", "font-size": "40px"}, 500);
+//     }
+// );
 
 // /*
 //     * Slideshow
